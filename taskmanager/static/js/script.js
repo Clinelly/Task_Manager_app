200335072e0279858afb4fdf79bf518a0bb5df55
Clinelly <swall289@gmail.com>
2023-01-20 14:49:51 +0000

    Add functionality to add tasks to site and database.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,3 +4,12 @@
     M.Sidenav.init(sidenav);
   });
 
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n:{done: "Select"}
+    });
+  
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
